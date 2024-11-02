c962837732d088f3c6b55fac1330b1b3c1a91c7d
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-02 21:18:29 +0800

    Perbaikan Migrasi dan Model Delivery Note

--- app/Models/DeliveryNote.php
+++ app/Models/DeliveryNote.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class DeliveryNote extends Model
 {
@@ -17,18 +18,49 @@ class DeliveryNote extends Model
         'client_id',
         'nomor_surat',
         'tanggal_pengiriman',
-        'waktu_pengiriman',
         'ship_to',
         'nomor_plat',
         'nama_driver',
         'palka',
         'status',
         'tanggal_sampai',
-        'waktu_sampai',
         'tanggal_bongkar',
-        'waktu_bongkar',
         'print'
     ];
+
+    protected $casts = [
+        'tanggal_pengiriman' => 'datetime',
+        'tanggal_sampai' => 'datetime',
+        'tanggal_bongkar' => 'datetime',
+        'print' => 'boolean',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            // Format: SJ-YYYYMM-INCREMENT
+            // Contoh: SJ-202405-0001
+
+            // Ambil nomor surat terbaru berdasarkan bulan dan tahun ini
+            $latestDeliveryNote = static::whereYear('created_at', now()->year)
+                ->whereMonth('created_at', now()->month)
+                ->latest()
+                ->first();
+
+            // Tentukan increment, mulai dari 1 jika belum ada nomor surat untuk bulan ini
+            $increment = $latestDeliveryNote ? (int)substr($latestDeliveryNote->nomor_surat, -4) + 1 : 1;
+
+            // Format nomor surat baru
+            $model->nomor_surat = sprintf(
+                'SJ-%s-%04d',
+                now()->format('Ym'),
+                $increment
+            );
+        });
+    }
+
 
     public function client()
     {
