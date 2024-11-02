30bed0a1e04674065094fef1a4cb3ea9439055d8
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-02 22:25:51 +0800

    Perbaikan Admin Panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,21 +2,29 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\DisableBladeIconComponents;
-use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use App\Filament\Resources\ClientResource;
+use App\Filament\Resources\DeliveryNoteResource;
+use App\Filament\Resources\OfficerResource;
+use App\Filament\Resources\UserResource;
 use Filament\Pages;
 use Filament\Panel;
-use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Filament\PanelProvider;
+use Filament\Pages\Dashboard;
+use Filament\Support\Colors\Color;
+use Filament\Navigation\NavigationItem;
+use Filament\Navigation\NavigationGroup;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Navigation\NavigationBuilder;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -30,10 +38,12 @@ class AdminPanelProvider extends PanelProvider
             ->registration()
             ->passwordReset()
             ->emailVerification()
+            ->sidebarCollapsibleOnDesktop()
             ->favicon(asset('images/logo.png'))
             ->brandLogo(asset('images/sidebar.png'))
             ->brandLogoHeight('49px')
             ->font('Comic Sans MS')
+            ->sidebarWidth('17rem')
             ->colors([
                 'primary' => '#005028',
                 'danger' => Color::Rose,
@@ -65,6 +75,34 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->navigation(function (NavigationBuilder $builder): NavigationBuilder {
+                return $builder->groups([
+                    NavigationGroup::make('Menu Utama')
+                        ->items([
+                            NavigationItem::make('dashboard')
+                                ->label(fn(): string => __('filament-panels::pages/dashboard.title'))
+                                ->url(fn(): string => Dashboard::getUrl())
+                                ->isActiveWhen(fn() => request()->routeIs('filament.admin.pages.dashboard'))
+                                ->icon('heroicon-o-home'),
+                        ])
+                        ->collapsible(false),
+                    NavigationGroup::make('Management Surat Jalan')
+                        ->items([
+                            ...DeliveryNoteResource::getNavigationItems(),
+                            NavigationItem::make('Buat Surat Jalan')
+                                ->label('Buat Surat Jalan')
+                                ->url(fn() => route('filament.admin.resources.Surat-Jalan.create'))
+                                ->icon('heroicon-o-document-plus')
+                                ->isActiveWhen(fn() => request()->routeIs('filament.admin.resources.Surat-Jalan.create')),
+                        ]),
+                    NavigationGroup::make('Management Pengguna')
+                        ->items([
+                            ...UserResource::getNavigationItems(),
+                            ...OfficerResource::getNavigationItems(),
+                            ...ClientResource::getNavigationItems(),
+                        ]),
+                ]);
+            });
     }
 }
