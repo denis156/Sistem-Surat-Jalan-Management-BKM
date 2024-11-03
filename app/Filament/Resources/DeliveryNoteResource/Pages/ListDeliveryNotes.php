4497c8a322317685666ad2e94eb865caa4749d91
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-04 01:46:17 +0800

    Penambahan Tabs Di ListDeliveryNote Panel Admin

--- app/Filament/Resources/DeliveryNoteResource/Pages/ListDeliveryNotes.php
+++ app/Filament/Resources/DeliveryNoteResource/Pages/ListDeliveryNotes.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\DeliveryNoteResource\Pages;
 
-use App\Filament\Resources\DeliveryNoteResource;
 use Filament\Actions;
+use App\Models\DeliveryNote;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\DeliveryNoteResource;
 
 class ListDeliveryNotes extends ListRecords
 {
@@ -17,6 +20,43 @@ class ListDeliveryNotes extends ListRecords
                 ->label('Tambah Surat Jalan')
                 ->color('primary')
                 ->icon('heroicon-o-plus'),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make('Semua')
+                ->label('Semua')
+                ->badge(DeliveryNote::count()),
+
+            'dibuat' => Tab::make('Dibuat')
+                ->label('Dibuat')
+                ->icon('heroicon-o-pencil')
+                ->badge(DeliveryNote::where('status', 'dibuat')->count())
+                ->badgeColor('secondary')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'dibuat')),
+
+            'dikirim' => Tab::make('Dikirim')
+                ->label('Dikirim')
+                ->icon('heroicon-o-truck')
+                ->badge(DeliveryNote::where('status', 'dikirim')->count())
+                ->badgeColor('warning')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'dikirim')),
+
+            'sampai' => Tab::make('Sampai')
+                ->label('Sampai')
+                ->icon('heroicon-o-check-circle')
+                ->badge(DeliveryNote::where('status', 'sampai')->count())
+                ->badgeColor('info')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'sampai')),
+
+            'selesai' => Tab::make('Selesai')
+                ->label('Selesai')
+                ->icon('heroicon-o-flag')
+                ->badge(DeliveryNote::where('status', 'selesai')->count())
+                ->badgeColor('success')
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('status', 'selesai')),
         ];
     }
 }
