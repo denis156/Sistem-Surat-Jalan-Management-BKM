b9cab28d14623391b539c7c96ad2e197539a04b7
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-05 20:09:55 +0800

    Perbaikan Tabel DeliveryNote Di Panel Field

--- app/Filament/Field/Resources/DeliveryNoteResource.php
+++ app/Filament/Field/Resources/DeliveryNoteResource.php
@@ -10,6 +10,7 @@ use Filament\Tables\Table;
 use App\Models\DeliveryNote;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Wizard;
+use Filament\Support\Enums\Alignment;
 use Filament\Support\Enums\ActionSize;
 use Illuminate\Database\Eloquent\Builder;
 use Filament\Forms\Components\Wizard\Step;
@@ -174,94 +175,176 @@ class DeliveryNoteResource extends Resource
                 Tables\Columns\TextColumn::make('index')
                     ->rowIndex()
                     ->label('No.'),
-                Tables\Columns\TextColumn::make('nomor_surat')
-                    ->label('Nomor Surat')
-                    ->searchable()
-                    ->sortable()
-                    ->copyable()
-                    ->copyMessage('Nomor surat berhasil disalin!')
-                    ->copyMessageDuration(1500)
-                    ->tooltip('Klik untuk menyalin')
-                    ->weight('bold'),
+                // Informasi Dasar
+                Tables\Columns\ColumnGroup::make('Informasi Dasar', [
+                    Tables\Columns\TextColumn::make('nomor_surat')
+                        ->label('Nomor Surat')
+                        ->searchable()
+                        ->sortable()
+                        ->copyable()
+                        ->copyMessage('Nomor surat berhasil disalin!')
+                        ->copyMessageDuration(1500)
+                        ->tooltip('Klik untuk menyalin')
+                        ->weight('bold'),
 
-                Tables\Columns\TextColumn::make('client.user.name')
-                    ->label('Klien')
-                    ->searchable()
-                    ->sortable(),
+                    Tables\Columns\TextColumn::make('status')
+                        ->badge()
+                        ->color(fn(string $state): string => match ($state) {
+                            'dibuat' => 'gray',
+                            'dikirim' => 'warning',
+                            'sampai' => 'info',
+                            'selesai' => 'success',
+                        })
+                        ->icon(fn(string $state): string => match ($state) {
+                            'dibuat' => 'heroicon-o-pencil',
+                            'dikirim' => 'heroicon-o-truck',
+                            'sampai' => 'heroicon-o-check-circle',
+                            'selesai' => 'heroicon-o-flag',
+                            default => 'heroicon-o-question-mark-circle'
+                        })
+                        ->searchable()
+                        ->sortable(),
 
-                Tables\Columns\TextColumn::make('items_sum_quantity')
-                    ->label('Jumlah Item')
-                    ->sum('items', 'quantity')
-                    ->sortable()
-                    ->badge()
-                    ->color('info'),
+                    Tables\Columns\IconColumn::make('print')
+                        ->label('Dicetak')
+                        ->boolean()
+                        ->trueIcon('heroicon-o-check-circle')
+                        ->falseIcon('heroicon-o-x-circle')
+                        ->trueColor('success')
+                        ->falseColor('danger'),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
 
-                Tables\Columns\TextColumn::make('fieldOfficer.user.name')
-                    ->label('Petugas Lapangan')
-                    ->searchable()
-                    ->toggleable(),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->label('Tgl. Dibuat')
-                    ->dateTime('d M Y H:i')
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                // Informasi Klien
+                Tables\Columns\ColumnGroup::make('Informasi Klien', [
+                    Tables\Columns\TextColumn::make('client.user.name')
+                        ->label('Nama PIC')
+                        ->searchable()
+                        ->sortable(),
 
-                Tables\Columns\TextColumn::make('roomOfficer.user.name')
-                    ->label('Petugas Ruangan')
-                    ->searchable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    Tables\Columns\TextColumn::make('client.company_name')
+                        ->label('Perusahaan PIC')
+                        ->searchable()
+                        ->sortable()
+                        ->toggleable(isToggledHiddenByDefault: true),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
 
-                Tables\Columns\TextColumn::make('ship_to')
-                    ->label('Tujuan')
-                    ->badge()
-                    ->color(fn(string $state): string => match ($state) {
-                        'gudang kota' => 'success',
-                        'gudang unaaha' => 'warning',
-                        'gudang kolaka' => 'danger',
-                    })
-                    ->searchable(),
+                // Informasi Petugas
+                Tables\Columns\ColumnGroup::make('Informasi Petugas', [
+                    Tables\Columns\TextColumn::make('fieldOfficer.user.name')
+                        ->label('Petugas Lapangan')
+                        ->searchable()
+                        ->toggleable(isToggledHiddenByDefault: true),
 
-                Tables\Columns\TextColumn::make('tanggal_pengiriman')
-                    ->label('Tgl. Dikirim')
-                    ->date('d M Y H:i')
-                    ->sortable(),
+                    Tables\Columns\TextColumn::make('roomOfficer.user.name')
+                        ->label('Petugas Ruangan')
+                        ->searchable()
+                        ->toggleable(isToggledHiddenByDefault: true),
 
-                Tables\Columns\TextColumn::make('warehouseOfficer.user.name')
-                    ->label('Petugas Gudang')
-                    ->searchable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    Tables\Columns\TextColumn::make('warehouseOfficer.user.name')
+                        ->label('Petugas Gudang')
+                        ->searchable()
+                        ->toggleable(isToggledHiddenByDefault: true),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
 
-                Tables\Columns\TextColumn::make('status')
-                    ->badge()
-                    ->color(fn(string $state): string => match ($state) {
-                        'dibuat' => 'gray',
-                        'dikirim' => 'warning',
-                        'sampai' => 'info',
-                        'selesai' => 'success',
-                    })
-                    ->icon(fn(string $state): string => match ($state) {
-                        'dibuat' => 'heroicon-o-pencil',
-                        'dikirim' => 'heroicon-o-truck',
-                        'sampai' => 'heroicon-o-check-circle',
-                        'selesai' => 'heroicon-o-flag',
-                        default => 'heroicon-o-question-mark-circle'
-                    })
-                    ->searchable()
-                    ->sortable(),
+                // Informasi Pengiriman
+                Tables\Columns\ColumnGroup::make('Informasi Pengiriman', [
+                    Tables\Columns\TextColumn::make('ship_to')
+                        ->label('Tujuan')
+                        ->badge()
+                        ->color(fn(string $state): string => match ($state) {
+                            'gudang kota' => 'success',
+                            'gudang unaaha' => 'warning',
+                            'gudang kolaka' => 'danger',
+                        })
+                        ->searchable(),
 
-                Tables\Columns\IconColumn::make('print')
-                    ->label('Dicetak')
-                    ->boolean()
-                    ->trueIcon('heroicon-o-check-circle')
-                    ->falseIcon('heroicon-o-x-circle')
-                    ->trueColor('success')
-                    ->falseColor('danger'),
+                    Tables\Columns\TextColumn::make('nama_driver')
+                        ->label('Supir')
+                        ->searchable()
+                        ->sortable(),
 
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->label('Tgl. Diupdate')
-                    ->dateTime('d M Y H:i')
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    Tables\Columns\TextColumn::make('nomor_plat')
+                        ->label('Plat')
+                        ->searchable()
+                        ->sortable(),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
+
+                // Informasi Item
+                Tables\Columns\ColumnGroup::make('Informasi Item', [
+                    Tables\Columns\TextColumn::make('items.name_item')
+                        ->label('Nama Item')
+                        ->listWithLineBreaks()
+                        ->bulleted(),
+
+                    Tables\Columns\TextColumn::make('items.quantity')
+                        ->label('Jumlah')
+                        ->listWithLineBreaks(),
+
+                    Tables\Columns\TextColumn::make('items.description')
+                        ->label('Kondisi')
+                        ->listWithLineBreaks(),
+
+                    Tables\Columns\TextColumn::make('items_sum_quantity')
+                        ->label('Jumlah Item')
+                        ->getStateUsing(function (DeliveryNote $record) {
+                            $total = 0;
+                            foreach ($record->items as $item) {
+                                if ($item->description === 'karung kurang') {
+                                    $total -= $item->quantity;
+                                } else {
+                                    $total += $item->quantity;
+                                }
+                            }
+                            return $total;
+                        })
+                        ->badge()
+                        ->color('info'),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
+
+                // Informasi Tanggal Dan Waktu
+                Tables\Columns\ColumnGroup::make('Informasi Tanggal Dan Waktu', [
+                    Tables\Columns\TextColumn::make('created_at')
+                        ->label('Tgl. Dibuat')
+                        ->dateTime('d M Y H:i')
+                        ->color('secondary')
+                        ->sortable(),
+
+                    Tables\Columns\TextColumn::make('tanggal_pengiriman')
+                        ->label('Tgl. Dikirim')
+                        ->color('warning')
+                        ->date('d M Y H:i')
+                        ->sortable(),
+
+                    Tables\Columns\TextColumn::make('tanggal_sampai')
+                        ->label('Tgl. Sampai')
+                        ->color('info')
+                        ->date('d M Y H:i')
+                        ->sortable(),
+
+                    Tables\Columns\TextColumn::make('tanggal_bongkar')
+                        ->label('Tgl. Selesai')
+                        ->color('success')
+                        ->date('d M Y H:i')
+                        ->sortable(),
+
+                    Tables\Columns\TextColumn::make('updated_at')
+                        ->label('Tgl. Update')
+                        ->dateTime('d M Y H:i')
+                        ->sortable()
+                        ->toggleable(isToggledHiddenByDefault: true),
+                ])
+                    ->alignment(Alignment::Center)
+                    ->wrapHeader(),
             ])
 
             ->filters([
