f18b8b1c17d3684af58300723b78e077bda859c6
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-08 00:44:32 +0800

    Pemindaha Style Dari Index Ke app.css Untuk LandingPage

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -17,46 +17,7 @@
     <style>
         /* ... your existing styles ... */
 
-        /* Add loading state styles */
-        .page-loading {
-            opacity: 0;
-            transition: opacity 0.3s ease-in;
-        }
-
-        .page-loaded {
-            opacity: 1;
-        }
-
-        /* Add animation classes */
-        @keyframes blob {
-            0% {
-                transform: translate(0px, 0px) scale(1);
-            }
-
-            33% {
-                transform: translate(30px, -50px) scale(1.1);
-            }
-
-            66% {
-                transform: translate(-20px, 20px) scale(0.9);
-            }
-
-            100% {
-                transform: translate(0px, 0px) scale(1);
-            }
-        }
-
-        .animate-blob {
-            animation: blob 7s infinite;
-        }
-
-        .animation-delay-2000 {
-            animation-delay: 2s;
-        }
-
-        .animation-delay-4000 {
-            animation-delay: 4s;
-        }
+        
     </style>
 </head>
 
