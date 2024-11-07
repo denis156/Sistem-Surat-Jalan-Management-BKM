94e7fdc4cbf58b34a930b92013c6d18f10232e3d
Denisa_13 <56766058+denis156@users.noreply.github.com>
2024-11-07 09:54:31 +0800

    Penghilangan Tombol Restore Dan Delete Dan Penambahan Logika Agar Surat Jalan Tidak Bisa Di edit Jika Statusnya Sudah Selesai

--- app/Filament/Warehouse/Resources/DeliveryNoteResource/Pages/EditDeliveryNote.php
+++ app/Filament/Warehouse/Resources/DeliveryNoteResource/Pages/EditDeliveryNote.php
@@ -2,20 +2,49 @@
 
 namespace App\Filament\Warehouse\Resources\DeliveryNoteResource\Pages;
 
-use App\Filament\Warehouse\Resources\DeliveryNoteResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Warehouse\Resources\DeliveryNoteResource;
 
 class EditDeliveryNote extends EditRecord
 {
     protected static string $resource = DeliveryNoteResource::class;
 
+    protected function beforeFill(): void
+    {
+        $this->ensureEditableStatus();
+    }
+
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
-            Actions\ForceDeleteAction::make(),
-            Actions\RestoreAction::make(),
+            Actions\Action::make('back')
+                ->label('Kembali')
+                ->url($this->getResource()::getUrl('index'))
+                ->color('secondary')
+                ->icon('heroicon-o-arrow-left'),
         ];
+    }
+
+    /**
+     * Ensure that the record cannot be edited if its status is "selesai".
+     */
+    protected function ensureEditableStatus(): void
+    {
+        // Cegah pengeditan jika status adalah "selesai"
+        if ($this->record->status === 'selesai') {
+            // Kirim notifikasi ke pengguna
+            Notification::make()
+                ->title('Tidak dapat mengedit')
+                ->body('Surat jalan ini tidak bisa diedit karena statusnya sudah "selesai".')
+                ->danger()
+                ->persistent()
+                ->duration(5000)
+                ->send();
+
+            // Redirect ke halaman daftar
+            $this->redirectRoute('filament.warehouse.resources.Penerimaan-Surat-Jalan.index');
+        }
     }
 }
